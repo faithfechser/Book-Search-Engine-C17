ea9c532da91e65e37678138724a59bebfdb3a754
Faith Fechser <faithfechser@gmail.com>
2023-08-02 14:23:15 -0600

    comment out unnecessary routing code

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,7 @@
-const router = require('express').Router();
-const userRoutes = require('./user-routes');
+// DONT NEED ROUTE
+// const router = require('express').Router();
+// const userRoutes = require('./user-routes');
 
-router.use('/users', userRoutes);
+// router.use('/users', userRoutes);
 
-module.exports = router;
+// module.exports = router;
