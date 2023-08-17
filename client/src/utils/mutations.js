482c19959349fd30bb58dbe137e0fa27bedfb085
Faith Fechser <faithfechser@gmail.com>
2023-08-17 09:44:12 -0600

    create mutation code

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,49 @@
+import { gql } from '@apollo/client';
+// add user
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                email
+                username
+            }
+        }
+    }
+`;
+// login
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                email
+                username
+            }
+        }
+    }
+`;
+// save book
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookId: String!) {
+        saveBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks
+        }
+    }
+`;
+// remove book
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks
+        }
+    }
+`;
