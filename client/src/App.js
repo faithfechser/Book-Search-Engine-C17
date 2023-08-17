14e95471fd6837288e32f2f8eeaa76aecdd86a58
Faith Fechser <faithfechser@gmail.com>
2023-08-17 07:33:28 -0600

    add apollo specific info

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+// add apollo specific info
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 function App() {
   return (
