b170b825c177a9229be63a6d0e73831c7429fc50
Faith Fechser <faithfechser@gmail.com>
2023-08-19 20:05:41 -0600

    deploy and debugging

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use('/graphql', expressMiddleware(server, {context: authMiddleware}))
 
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '../client/dist')))
+  app.use(express.static(path.join(__dirname, './client/dist')))
   app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, './client/dist/index.html'))
   })
