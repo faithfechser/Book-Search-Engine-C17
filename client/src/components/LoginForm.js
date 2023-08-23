acf86a5eea77bdea80d186bd33d466beaef63449
Faith Fechser <faithfechser@gmail.com>
2023-08-22 21:44:04 -0600

    fix login form to match graphQL format

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,15 +1,16 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/mutations';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-
+  const [loginUser, { data, loading, error }] = useMutation(LOGIN_USER);
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -26,13 +27,9 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
+      const response = await loginUser({ variables: userFormData });
+      console.log(response)
+      const { token, user } = response.data.loginUser;
       console.log(user);
       Auth.login(token);
     } catch (err) {
