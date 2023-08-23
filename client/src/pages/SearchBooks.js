98a34ce3b70c941d96106eaaed23aac615baa968
Faith Fechser <faithfechser@gmail.com>
2023-08-23 01:04:27 -0600

    book saving debug

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -70,11 +70,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const response = await saveBook({variables: {bookData: {...bookToSave}}});
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
